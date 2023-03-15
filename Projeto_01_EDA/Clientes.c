cc6447865b2aaa71684ee519557682034c877cd4
Ismael_pc2 <ismaeltendarm@gmail.com>
2023-03-15 17:26:56 +0000

    Criado ficheiro Dados.txt

---
+++ Projeto_01_EDA/Clientes.c
@@ -0,0 +1,14 @@
+/*****************************************************************//**
+ * \file   Clientes.c
+ * \brief  Tudo sobre os clientes
+ * 
+ * \author Ismael Morais a25964
+ * \date   March 2023
+ *********************************************************************/
+
+typedef struct Clientes {
+	char nome[50];
+	double saldo[10000];
+
+
+}Clientes;
--- Projeto_01_EDA/Programa.c
+++ Projeto_01_EDA/Programa.c
@@ -2,9 +2,27 @@
  * \file   Programa.c
  * \brief  
  * 
- * \author Ismael Morais  a25964
- * \date   09 March 2023
+ * \author Ismael Morais a25964
+ * \date   March 2023
  *********************************************************************/
 
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include <string.h>
 
+void main() {
+	FILE* util;
+	
+	util = fopen("Clinete.txt", "w");
+
+	if (util != NULL) {
+		fprintf(util, "hytgfivjh\n");
+		fputs("23\n", util);
+
+		fclose(util);
+	}
+	else {
+		printf("Erro ao abrir o ficheiro.\n");
+	}
+
+}
